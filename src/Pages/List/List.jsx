b118ae603a29bf22fe64fb7a1b0f0b44229f144e
Cuong98 <cuongkieu2898@gmail.com>
2022-05-12 11:13:18 +0700

    completed search Page UI

--- src/Pages/List/List.jsx
+++ src/Pages/List/List.jsx
@@ -5,6 +5,8 @@ import { useLocation } from "react-router-dom"
 import { useState } from "react";
 import { format } from "date-fns";
 import { DateRange } from "react-date-range";
+import LIMIT_NUMBER from "../../consts/LIMIT_NUMBER";
+import SearchList from "../../components/searchpage/SearchList";
 
 
 const List = () => {
@@ -41,12 +43,51 @@ const List = () => {
               {isOpenEndDate &&<DateRange onChange={(item) => setDate([item.selection])} minDate={new Date()} ranges={date}/>}
             </div>
             <div className="ls-item">
+              <label htmlFor="">Options</label>
+              <div className="ls-options">
+
+              
+              <div className="ls-option-item">
+                <span className="ls-option-text">
+                  Giá từ <small>mỗi đêm</small>
+                </span>
+                <input type="text" className="ls-options-input" />
+              </div>
+              <div className="ls-option-item">
+                <span className="ls-option-text">
+                  Tới giá <small>mỗi đêm</small>
+                </span>
+                <input type="text" className="ls-options-input" />
+              </div>
+              <div className="ls-option-item">
+                <span className="ls-option-text">
+                  Người lớn
+                </span>
+                <input type="number" min={LIMIT_NUMBER.adult} className="ls-options-input" value={options.adult}/>
+              </div>
+              <div className="ls-option-item">
+                <span className="ls-option-text">
+                  Trẻ em 
+                </span>
+                <input type="number" min={LIMIT_NUMBER.children} className="ls-options-input" value={options.children}/>
+              </div>
+              <div className="ls-option-item">
+                <span className="ls-option-text">
+                  Phòng 
+                </span>
+                <input type="number" min={LIMIT_NUMBER.room} className="ls-options-input" value={options.room}/>
+              </div>
+            </div>
+            </div>
+            <div className="ls-item">
               <label htmlFor="">Nghỉ 30 đêm</label>
               <input type="text" name="" id="" placeholder={`${options.adult} adult - ${options.children} children - ${options.room} room`}/>
             </div>
+            <button>Tìm kiếm</button>
           </div>
-          <div className="list-result"></div>
-          
+          <div className="list-result">
+            <SearchList />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/searchpage/SearchList.jsx
@@ -0,0 +1,215 @@
+import "../../Stylesheets/components/searchList.css";
+
+const SearchList = () => {
+  return (
+      <>
+
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    <div className="search-item">
+      <img
+        src="https://t-cf.bstatic.com/xdata/images/hotel/square200/357381809.webp?k=6438cf4b657b3235fae8d1e6e67ac3f2b0ab24c54ba081bb13351c83051269b4&o=&s=1"
+        alt=""
+        className="si-img"
+      />
+      <div className="si-desc">
+        <h1 className="si-title">Cỏ homestay Vũng Tàu</h1>
+        <span className="si-distance">Cách trung tâm 1,2km</span>
+        <span className="si-taxi-op">Miễn phí taxi sân bay</span>
+        <span className="si-subtitle">Cách bãi biển 950m</span>
+        <span className="si-features">
+          1 căn hộ nguyên căn • 2 phòng ngủ • 2 phòng tắm
+        </span>
+        <span className="si-cancel-op">
+          Miễn Phí hủy phòng • Không cần thanh toán trước
+        </span>
+        <span className="cancel-op-subtitle">
+          Bạn có thể hủy sau, nên hãy đặt ngay hôm nay
+        </span>
+      </div>
+      <div className="si-details">
+        <div className="si-rating">
+          <span>Xuất sắc</span>
+          <button>8.9</button>
+        </div>
+        <div className="si-detail-text">
+          <span className="si-price">2.000.000vnđ</span>
+          <span className="si-tax-op">Đã bao gồm thuế và phí</span>
+          <button className="si-check-btn">Xem chi tiết</button>
+        </div>
+      </div>
+      
+    </div>
+    </>
+  );
+};
+
+export default SearchList;
